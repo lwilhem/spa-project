ce9aca6359fb4d452f80a4d70cd238ab4d616298
Wilhem Lecanu <wilhem.lecanu@edu.devinci.fr>
2022-01-31 15:04:49 +0100

    :tada: initial commit

---
+++ login.php
@@ -0,0 +1,35 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>RENDU POO - Login</title>
+</head>
+<body>
+<form action="" method="post">
+    <label for="register-name">
+        Name
+        <input type="text" name="register-username" id="register-username">
+    </label>
+    <label for="register-lastname">
+        LastName
+        <input type="text" name="register-lastname" id="register-lastname">
+    </label>
+    <label for="register-email">
+        E-Mail
+        <input type="email" name="register-email" id="register-email">
+    </label>
+    <label for="register-password">
+        Password
+        <input type="password" name="register-password" id="register-password">
+    </label>
+    <label for="register-password-check">
+        Verify Password
+        <input type="password" name="register-password-check" id="register-password-check">
+    </label>
+    <input type="submit">
+</form>
+</body>
+</html>
