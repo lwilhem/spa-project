ae57fd010ffd3ae61185af92335714e0b895a5fc
Wilhem Lecanu <wilhem.lecanu@edu.devinci.fr>
2022-01-31 15:20:12 +0100

    :construction: fixed project organization

---
+++ views/login.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>RENDU POO - Login</title>
+</head>
+<body>
+<form action="" method="post">
+    <input type="text" name="register-firstname" id="register-firstname">
+    <input type="text" name="register-lastname" id="register-lastname">
+    <input type="email" name="register-email" id="register-email">
+    <input type="password" name="register-password" id="register-password">
+    <input type="password" name="register-password-check" id="register-password-check">
+    <input type="submit" value="Create your account">
+</form>
+</body>
+</html>
