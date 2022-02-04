706206d49412131dcdcc6d489819075123bfb3a0
Wilhem Lecanu <wilhem.lecanu@edu.devinci.fr>
2022-02-04 17:15:58 +0100

    :construction: WIP Session Parameters

--- objects/auth.php
+++ objects/auth.php
@@ -1,18 +1,1 @@
 <?php
-
-class Auth extends User
-{
-    public bool $isAdmin;
-    public int $userid;
-
-    public function adminCheck($isAdmin)
-    {
-        $this->isAdmin = $isAdmin;
-
-        if($isAdmin === true){
-            $_SESSION['isAdmin'] = true;
-        }else{
-            $_SESSION['isAdmin'] = false;
-        }
-    }
-}
